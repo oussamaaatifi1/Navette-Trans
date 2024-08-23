ecb6ded56d4a313440ce0009ace72fc132b049c2
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-23 12:47:54 +0100

    j'ai créer projet aussi install dependecies cette projet

---
+++ Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/PlatformTransportApplication.java
@@ -0,0 +1,13 @@
+package com.platformtrasnport.platformtransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformTransportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformTransportApplication.class, args);
+    }
+
+}
