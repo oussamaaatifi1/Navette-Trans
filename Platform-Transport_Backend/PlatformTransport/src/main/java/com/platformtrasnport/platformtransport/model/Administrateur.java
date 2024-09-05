132da0ec40ac5d9355fb75eeed18cef3b2d63c20
oussamaaatifi1 <nisowpro@gmail.com>
2024-09-05 09:59:48 +0100

    j'ai ajouter annontation DiscriminatValue pour identifier name table à database

--- Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/model/Administrateur.java
+++ Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/model/Administrateur.java
@@ -2,6 +2,16 @@ package com.platformtrasnport.platformtransport.model;
 
 import jakarta.persistence.Entity;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@DiscriminatorValue("Administrateur")
 public class Administrateur extends Utilisateur {
 }
