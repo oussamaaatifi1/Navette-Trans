954b55b9b3bfd2eb9ab09e3f5514929d5930834d
oussamaaatifi1 <nisowpro@gmail.com>
2024-09-05 10:12:44 +0100

    package auth security

---
+++ Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.platformtrasnport.platformtransport.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String nom;
+    private String email;
+    private String password;
+}
