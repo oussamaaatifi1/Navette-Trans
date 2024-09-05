8b6082c338850ed88f0f0b89ead45bc7cab38c10
oussamaaatifi1 <nisowpro@gmail.com>
2024-09-05 09:58:29 +0100

    added role cette projet

--- Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/model/enul/Role.java
+++ Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/model/enul/Role.java
@@ -1,4 +1,17 @@
 package com.platformtrasnport.platformtransport.model.enul;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    EMPLOYE,
+    EMPLOYEUR,
+    ADMIN,
+    PARTICULIER,
+    ;
+
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
 }
