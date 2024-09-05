6e3af9058e18a548111b4843306fda616386b4fb
oussamaaatifi1 <nisowpro@gmail.com>
2024-09-05 10:02:58 +0100

    ajouter fk transaction pour chaque reservation

--- Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/model/Reservation.java
+++ Platform-Transport_Backend/PlatformTransport/src/main/java/com/platformtrasnport/platformtransport/model/Reservation.java
@@ -1,26 +1,37 @@
 package com.platformtrasnport.platformtransport.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+
 public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Temporal(TemporalType.DATE)
-    private Date dateReservation;
+    private LocalDate dateReservation;
 
     @ManyToOne
-    @JoinColumn(name = "employe_id")
+    @JoinColumn(name = "employee_id")
     private Employe employe;
 
     @ManyToOne
     @JoinColumn(name = "offre_id")
     private OffreTransport offre;
 
+    @OneToOne
+    @JoinColumn(name = "transaction_id")
+    private Transaction transaction;
 
 }
